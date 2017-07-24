0980d1eb0fc17d4fd544855300a0d902222541fd
Martin Ankerl <martin.ankerl@dynatrace.com>
2017-07-24 12:44:11 +0200

    better convert: crops image, samples chroma

--- img/mega/convert.sh
+++ img/mega/convert.sh
@@ -1,1 +1,4 @@
-find . -iname "*.jpg" -exec convert -resize 1280x -strip -interlace Plane -quality 75% {} ../big/{} \;
+#!/bin/bash
+
+# also see https://developers.google.com/speed/docs/insights/OptimizeImages
+find . -iname "*.jpg" -exec convert -resize 1280x -gravity center -crop 1280x720+0+0 -sampling-factor 4:2:0  -strip -interlace JPEG -quality 75% {} ../big/{} \;
